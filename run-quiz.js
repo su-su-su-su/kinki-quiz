8bc1ef45a1b0078fe13e36c4c4b9a32811abd6c6
su-su-su-su <suzukikoji0503@gmail.com>
2025-07-14 11:13:32 +0900

    県庁所在地のクイズを作成委

---
+++ run-quiz.js
@@ -0,0 +1,25 @@
+import readline from "readline";
+import quizzer from "./index.js";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const question = quizzer.getQuiz();
+
+rl.question(
+  `問題: ${question.pref}の県庁所在地はどこでしょう？\n> `,
+  (userAnswer) => {
+    const isCorrect = quizzer.checkAnswer(question.pref, userAnswer);
+
+    if (isCorrect) {
+      console.log("正解です！おめでとうございます！");
+    } else {
+      const correctAnswer = quizzer.getCorrectAnswer(question.pref);
+      console.log(`残念、不正解。正しい答えは「${correctAnswer}」です。`);
+    }
+
+    rl.close();
+  }
+);
